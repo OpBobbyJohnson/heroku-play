c4a0fa8211aa91c9a30ad7daa0656439ad8be6d9
Dylan <dylanm@udel.edu>
2019-09-16 13:45:50 -0400

    hopefully this fixed it

--- api.php
+++ api.php
@@ -31,6 +31,7 @@
     foreach ($entityBody as &$value){
         // $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 10";
         $query = "SELECT rack, words FROM racks WHERE rack=\".$value.\"";
+        echo $query;
         //this next line could actually be used to provide user_given input to the query to 
         //avoid SQL injection attacks
         $statement = $dbhandle->prepare($query);
