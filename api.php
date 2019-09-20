840d205237fa3560844682d721299eabbc15834a
Dylan <dylanm@udel.edu>
2019-09-20 11:11:16 -0400

    gota get those bingos

--- api.php
+++ api.php
@@ -72,7 +72,8 @@
     //making to this line means everything was great with this request
     header('HTTP/1.1 200 OK');
     //this lets the browser know to expect json
-    header('Content-Type: application/json');
+    // header('Content-Type: application/json');
+    header('Content-Type: text/html');
     //this creates json and gives it back to the browser
     // echo json_encode($results);
     echo $racks;
